0c01bd743db74d8c641d94236365b3d76218d351
KabriAcid <kabriacid01@gmail.com>
2025-07-03 23:55:15 +0100

    Add initial components for dashboard layout and user utilities

---
+++ components/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Page title is set dynamically -->
+    <title><?= set_title('Cerebro' ?? null) ?></title>
+    <link rel="icon" href="/cerebro/assets/img/logo.png" type="image/x-icon">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="/cerebro/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/cerebro/assets/css/style.css">
+    
+
+    <style>
+        body {
+            font-family: 'Raleway', sans-serif;
+        }
+    </style>
+
+</head>
---
+++ components/navbar.php
@@ -0,0 +1,25 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-white bg-white" id="mainNavbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/cerebro/index.php">
+                <img src="/cerebro/assets/img/logo.png" class="favicon" alt="Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active font-weight-bold" href="/cerebro/index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bold" href="/cerebro/public/contact.php">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link font-weight-bold" href="/cerebro/public/about.php">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
